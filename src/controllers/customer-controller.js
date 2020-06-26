5b63db95f159d644121e6f405c2727287f5760a7
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-06-26 09:44:16 -0300

    Encriptando a senha

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require('../validators/fluent-validator')
 const repository = require('../repositories/customer-repository')
+const md5 = require('md5')
 
 exports.get = async (req, res, next) => {
   try {
@@ -32,7 +33,11 @@ exports.post = async (req, res, next) => {
       return
     }
 
-    await repository.create(req.body)
+    await repository.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: md5(req.body.password + global.SALT_KEY)
+    })
     res.status(201).send({ message: 'Cliente cadastrado com sucesso!' })
   } catch (e) {
     res.status(400).send({ message: 'Falha ao cadastrar o cliente', data: e })
